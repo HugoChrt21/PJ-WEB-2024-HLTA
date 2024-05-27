b98a09ccd68247d35d780596545b9ce6ec99218c
Alexis Morieux <131675283+alexismrx21@users.noreply.github.com>
2024-05-27 11:32:17 +0200

    Add files via upload

---
+++ act_sportive.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Votre Page Web</title>
+    <link rel="stylesheet" href="act_sportive.css">
+</head>
+
+<body>
+    <header>
+        <div class="slogan">
+            <h1>Sportify: Consultation Sportive</h1>
+        </div>
+        <div class="logo">
+            <img src="" alt="Logo">
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li>
+                <a href="#" class="active">Tout Parcourir</a> <!-- Ajout de la classe active ici -->
+                <ul class="dropdown">
+                    <li><a href="#">Activités sportives</a></li>
+                    <li><a href="#">Les Sports de compétition</a></li>
+                    <li><a href="#">Salle de sport Omnes</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Recherche</a></li>
+            <li><a href="#">Rendez-vous</a></li>
+            <li><a href="#">Votre Compte</a></li>
+        </ul>
+    </nav>
+    <div class="wrapper">
+        <!-- Contenu principal de la page -->
+    </div>
+</body>
+
+</html>
