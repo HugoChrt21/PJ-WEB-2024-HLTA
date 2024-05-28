c171333990d3d1d1ba79d8ac3bb3e5c0a89faec4
thomassbgne <132274990+thomassbgne@users.noreply.github.com>
2024-05-28 17:29:12 +0200

    Update salle_sport.php

--- salle_sport.php
+++ salle_sport.php
@@ -19,18 +19,18 @@
     </header>
     <nav>
         <ul>
-            <li><a href="#">Accueil</a></li>
+            <li><a href="accueil.php">Accueil</a></li>
             <li>
                 <a href="#" class="active">Tout Parcourir</a> <!-- Ajout de la classe active ici -->
                 <ul class="dropdown">
-                    <li><a href="#">Activités sportives</a></li>
-                    <li><a href="#">Les Sports de compétition</a></li>
-                    <li><a href="#">Salle de sport Omnes</a></li>
+                    <li><a href="act_sportive.php">Activités sportives</a></li>
+                    <li><a href="sport_compet.php">Les Sports de compétition</a></li>
+                    <li><a href="salle_sport.php">Salle de sport Omnes</a></li>
                 </ul>
             </li>
-            <li><a href="#">Recherche</a></li>
+            <li><a href="recherche.php">Recherche</a></li>
             <li><a href="#">Rendez-vous</a></li>
-            <li><a href="#">Votre Compte</a></li>
+            <li><a href="compte.php">Votre Compte</a></li>
         </ul>
     </nav>
     <div class="wrapper">
