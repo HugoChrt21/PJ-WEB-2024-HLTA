510406d5c334d55421d832c8782ec31ca6fa4857
HugoChrt21 <131973133+HugoChrt21@users.noreply.github.com>
2024-05-30 23:41:17 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -18,34 +18,21 @@ session_start(); // Commencez la session
 
 <?php
 
-
-$user = "root";
-$psd = "root";
-$db = "mysql:host=localhost;dbname=Sportify";
+// Informations de connexion à la base de données
+$serveur = "localhost:3307";
+$utilisateur = "root";
+$mot_de_passe = "123";
+$base_de_donnees = "Sportify";
 
 try {
-    $cx = new PDO($db, $user, $psd);
+    // Connexion à la base de données
+    $cx = new PDO("mysql:host=$serveur;dbname=$base_de_donnees", $utilisateur, $mot_de_passe);
     $cx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Une erreur est survenue lors de la connexion : " . $e->getMessage() . "</br>";
+    $error_message = "Une erreur est survenue lors de la connexion : " . $e->getMessage();
+    echo "<script>console.error('" . $error_message . "');</script>";
     die();
 }
-
-
-// $serveur = "localhost:3307";
-// $utilisateur = "root";
-// $mot_de_passe = "123";
-// $base_de_donnees = "Sportify";
-
-// try {
-//     // Connexion à la base de données
-//     $cx = new PDO("mysql:host=$serveur;dbname=$base_de_donnees", $utilisateur, $mot_de_passe);
-//     $cx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     $error_message = "Une erreur est survenue lors de la connexion : " . $e->getMessage();
-//     echo "<script>console.error('" . $error_message . "');</script>";
-//     die();
-// }
 
 $error_message = '';
 $welcome_message = '';
@@ -66,10 +53,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
             $welcome_message = "Bienvenue !";
             // Stockez l'adresse e-mail de l'utilisateur dans la session
             $_SESSION['email'] = $email;
-            $_SESSION['type'] = $user['type'];
-            echo "<script>console.error('" . $user['type'] . "');</script>";
-
-
             // Redirigez l'utilisateur vers la page compte.php
             header("Location: compte.php");
             exit(); // Assurez-vous que le script s'arrête après la redirection
@@ -96,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
         <ul>
             <li><a href="accueil.php">Accueil</a></li>
             <li>
-                <a href="#">Tout Parcourir</a>
+                <a href="#" class="active">Tout Parcourir</a>
                 <ul class="dropdown">
                     <li><a href="act_sportive.php">Activités sportives</a></li>
                     <li><a href="sport_compet.php">Les Sports de compétition</a></li>
@@ -105,22 +88,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
             </li>
             <li><a href="recherche.php">Recherche</a></li>
             <li><a href="#">Rendez-vous</a></li>
-            <li><a href="compte.php " class="active">Votre Compte</a></li>
+            <li><a href="connexion.php">Votre Compte</a></li>
         </ul>
     </nav>
     <div class="wrapper">
         <div class="login-container">
             <h2>Connexion</h2>
             <form method="post" action="">
-                <input type="hidden" name="login" value="1">
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-                <br>
-                <label for="password">Mot de passe:</label>
-                <input type="password" id="password" name="password" required>
-                <br>
-                <input type="submit" value="Connexion">
-            </form>
+            <input type="hidden" name="login" value="1">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <br>
+            <label for="password">Mot de passe:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <button class="connectBtn" type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" fill="white">
+            <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/>
+        </svg>
+        Connexion
+    </button>
+</form>
+
             <p><a href="inscription.php" style="color: blue;">Vous n'avez pas de compte ?</a></p>
         </div>
     </div>
